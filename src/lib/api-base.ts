7d68c721b91fc06378dacf64531fac1afdcd78f0
Letto <karvatwellinton@hotmail.com>
2025-09-15 10:07:54 -0300

    ngrok

--- src/lib/api-base.ts
+++ src/lib/api-base.ts
@@ -1,5 +1,5 @@
 // src/lib/api-base.ts
 export function getApiBaseUrl() {
   // Use SOMENTE o ngrok (inclua /api/v1)
-  return "http://127.0.0.1:5000/api/v1";
+  return "https://8831e45afbe6.ngrok-free.app/api/v1";
 }
